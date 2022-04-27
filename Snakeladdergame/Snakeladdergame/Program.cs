d15a91eb5e229dd5cc68da256ef116b907db4e79
snehasonwane <snehasonwane01@gmail.com>
2022-04-28 00:55:38 +0530

    Zero position

---
+++ Snakeladdergame/Snakeladdergame/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome SnakeLadder game!");
+public class program
+{
+    public static void Main(String[] args)
+    {
+        SnakeGame game = new SnakeGame();
+        int start = game.dieRoll();
+        Console.WriteLine("Welcome in the Game, Your Position is: " + "****" + start + "****");
+    }
+}
+public class SnakeGame
+    {
+        const int position = 0;
+        public int dieRoll()
+        { 
+          return position;
+        }
+        
+    }
