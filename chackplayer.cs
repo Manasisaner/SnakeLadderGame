7f71befcd166c74e7a8d311258959a385b24c2c9
Manasisaner <102273402+Manasisaner@users.noreply.github.com>
2022-05-01 22:30:15 +0530

    Add files via upload

---
+++ chackplayer.cs
@@ -0,0 +1,34 @@
+﻿namespace SnakeLadderGame
+{
+    public class SnakeGame
+    {
+        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        int position = 0;
+        public int dieRoll()
+        {
+            Random random = new Random();
+            int diePosition = random.Next(1, 7);
+            return diePosition;
+        }
+        public void gamePlaying()
+        {
+            Random random = new Random();
+            int roll = random.Next(0, 3);
+
+            switch (roll)
+            {
+                case NO_PLAY:
+                    this.position += 0;
+                    break;
+                case SNAKE:
+                    this.position -= this.dieRoll();
+                    break;
+                case LADDER:
+                    this.position += this.dieRoll();
+                    break;
+            }
+
+        }
+
+    }
+}
